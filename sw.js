b6da4698a492cfeccaa64b292dea1a15822cf77a
Osvelazq <osvelazq@gmail.com>
2026-07-19 12:11:58 -0400

    Refactor service worker caching logic

--- sw.js
+++ sw.js
@@ -1,10 +1,14 @@
-const cacheName = 'pitch-tracker-v1';
-const filesToCache = ['index.html', 'manifest.json'];
+const CACHE_NAME = 'pitch-tracker-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './manifest.json'
+];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(filesToCache)));
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
